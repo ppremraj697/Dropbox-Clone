a00e706cbe6670c63868e76c3a6c8c77be036138
Premraj Patil <ppremraj697@gmail.com>
2024-03-23 23:57:17 +0530

    Implemented Table Wrapper Component

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -1,10 +1,14 @@
 import { FileType } from "@/typings"
 import { Button } from "../ui/button"
+import { columns } from "./columns"
+import { DataTable } from "./Table"
 
 function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
     return (
         <div>
             <Button>Sort By...</Button>
+
+            <DataTable columns={columns} data={skeletonFiles} />
         </div>
     )
 }
